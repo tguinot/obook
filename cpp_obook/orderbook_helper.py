34016a981037c96045c7a56a736110ef6d546348
Quantum Labs <thomas.guinot@gmail.com>
2021-01-04 21:25:42 +0000

    Add helper functions

--- cpp_obook/orderbook_helper.py
+++ cpp_obook/orderbook_helper.py
@@ -8,6 +8,30 @@ class RtOrderbookWriter(orderbook_wrapper.OrderbookWriter):
 		print("Instantiated writer ")
 		self.init_shm(path)
 
+	def set_bid_quantity_at(self, price, quantity):
+		frac_price = Fraction.from_float(price)
+		frac_quantity = Fraction.from_float(quantity)
+		return super().set_quantity_at(True, frac_quantity.numerator, frac_quantity.denominator, frac_price.numerator, frac_price.denominator)
+
+	def set_ask_quantity_at(self, price, quantity):
+		frac_price = Fraction.from_float(price)
+		frac_quantity = Fraction.from_float(quantity)
+		return super().set_quantity_at(False, frac_quantity.numerator, frac_quantity.denominator, frac_price.numerator, frac_price.denominator)
+
+	def snapshot_bids(self, max_limit=10):
+		raw_result = super().snapshot_bids(max_limit)
+		result = []
+		return [(float(Fraction(*price)), float(Fraction(*qty))) for price, qty in raw_result]
+
+	def snapshot_asks(self, max_limit=10):
+		raw_result = super().snapshot_asks(max_limit)
+		result = []
+		return [(float(Fraction(*price)), float(Fraction(*qty))) for price, qty in raw_result]
+
+	def first_price(self, side):
+		price = super().first_price(side)
+		return float(Fraction(*price))
+
 class RtOrderbookReader(orderbook_wrapper.OrderbookReader):
 	def __init__(self, path):
 		super().__init__()
