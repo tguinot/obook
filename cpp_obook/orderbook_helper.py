72b481220ac4e81a22ec5b352f25ba0ec9107865
Quantum Labs <thomas.guinot@gmail.com>
2021-01-28 09:50:35 +0000

    Simplify helper classes, add basic consistency test

--- cpp_obook/orderbook_helper.py
+++ cpp_obook/orderbook_helper.py
@@ -2,7 +2,37 @@ from build import orderbook_wrapper
 from fractions import Fraction
 import random
 
-class RtOrderbookWriter(orderbook_wrapper.OrderbookWriter):
+
+class CommonOrderBookClass:
+	def snapshot_bids(self, max_limit=10):
+		raw_result = super().snapshot_bids(max_limit)
+		return [(float(Fraction(*price)), float(Fraction(*qty))) for price, qty in raw_result]
+
+	def snapshot_asks(self, max_limit=10):
+		raw_result = super().snapshot_asks(max_limit)
+		return [(float(Fraction(*price)), float(Fraction(*qty))) for price, qty in raw_result]
+
+	def first_price(self, side):
+		price = super().first_price(side)
+		return float(Fraction(*price))
+
+	def is_sound(self, max_limit=100):
+		bids = self.snapshot_bids(max_limit)
+		asks = self.snapshot_asks(max_limit)
+		if not bids == sorted(bids, key=lambda x: x[0], reverse=True):
+			return False
+		if not asks == sorted(asks, key=lambda x: x[0]):
+			return False
+		return True
+
+
+class RtOrderbookReader(orderbook_wrapper.OrderbookReader, CommonOrderBookClass):
+	def __init__(self, path):
+		super().__init__()
+		self.init_shm(path)
+
+	
+class RtOrderbookWriter(orderbook_wrapper.OrderbookWriter, CommonOrderBookClass):
 	def __init__(self, path):
 		super().__init__()
 		print("Instantiated writer ")
@@ -17,36 +47,3 @@ class RtOrderbookWriter(orderbook_wrapper.OrderbookWriter):
 		frac_price = Fraction.from_float(price)
 		frac_quantity = Fraction.from_float(quantity)
 		return super().set_quantity_at(False, frac_quantity.numerator, frac_quantity.denominator, frac_price.numerator, frac_price.denominator)
-
-	def snapshot_bids(self, max_limit=10):
-		raw_result = super().snapshot_bids(max_limit)
-		result = []
-		return [(float(Fraction(*price)), float(Fraction(*qty))) for price, qty in raw_result]
-
-	def snapshot_asks(self, max_limit=10):
-		raw_result = super().snapshot_asks(max_limit)
-		result = []
-		return [(float(Fraction(*price)), float(Fraction(*qty))) for price, qty in raw_result]
-
-	def first_price(self, side):
-		price = super().first_price(side)
-		return float(Fraction(*price))
-
-class RtOrderbookReader(orderbook_wrapper.OrderbookReader):
-	def __init__(self, path):
-		super().__init__()
-		self.init_shm(path)
-
-	def snapshot_bids(self, max_limit=10):
-		raw_result = super().snapshot_bids(max_limit)
-		result = []
-		return [(float(Fraction(*price)), float(Fraction(*qty))) for price, qty in raw_result]
-
-	def snapshot_asks(self, max_limit=10):
-		raw_result = super().snapshot_asks(max_limit)
-		result = []
-		return [(float(Fraction(*price)), float(Fraction(*qty))) for price, qty in raw_result]
-
-	def first_price(self, side):
-		price = super().first_price(side)
-		return float(Fraction(*price))
