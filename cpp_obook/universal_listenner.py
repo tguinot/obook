cd23c1a57c4473aa58dabb99e476cedff4789491
Quantum Labs <thomas.guinot@gmail.com>
2021-02-01 16:15:45 +0000

    Simplify listenner logic

--- cpp_obook/universal_listenner.py
+++ cpp_obook/universal_listenner.py
@@ -6,10 +6,11 @@ import time
 import os
 
 class UniversalFeedListenner(zerorpc.Client):
-    def __init__(self, addr, zmq_port, relayer_port, markets, on_orderbook_update=None, on_trade_update=None):
+    def __init__(self, relayer_addr, zmq_port, relayer_port, exchange, instrument, on_orderbook_update=None, on_trade_update=None):
         super().__init__()
-        self.markets = markets
-        self.connect(f"tcp://{addr}:{relayer_port}")
+        self.exchange = exchange
+        self.instrument = instrument
+        self.connect(f"tcp://{relayer_addr}:{relayer_port}")
         self.on_orderbook_update = on_orderbook_update
         self.on_trade_update = on_trade_update
         self.setup_zmq()
@@ -24,11 +25,10 @@ class UniversalFeedListenner(zerorpc.Client):
         self.zmq_socket = self.context.socket(zmq.SUB)
 
     def subscribe(self):
-        for market in self.markets:
-            port = self.subscribe_orderbook('FTX', market)
-            print("Subscribing to orderbook on port", port)
-            self.zmq_socket.connect("tcp://{}:{}".format("127.0.0.1", self.zmq_port))
-            self.zmq_socket.setsockopt_string(zmq.SUBSCRIBE, "")
+        port = self.subscribe_orderbook(self.exchange, self.instrument)
+        print("Subscribing to orderbook on port", port)
+        self.zmq_socket.connect("tcp://{}:{}".format("127.0.0.1", self.zmq_port))
+        self.zmq_socket.setsockopt_string(zmq.SUBSCRIBE, "")
 
     def listen(self):
         while True:
