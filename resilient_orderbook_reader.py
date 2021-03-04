f21aaf3076628540f6e7c7e45c748162640fbce8
Quantum Labs <thomas.guinot@gmail.com>
2021-03-04 11:51:07 +0000

    Add resilient orderbook reader

---
+++ resilient_orderbook_reader.py
@@ -0,0 +1,17 @@
+from liborderbook.orderbook_helper import RtOrderbookReader
+from models import OrderbookRecord, Currency, Exchange, Service
+
+class ResilientOrderbookReader(RtOrderbookReader):
+
+    def __init__(self, exchange, instrument):
+        self.exchange, self.instrument = exchange, instrument
+        shm = self.get_orderbook_shm()
+        super().__init__(shm)
+
+    def get_orderbook_shm(self):
+        orderbook_details = Service.get((Service.name == 'OrderbookFeeder') & (Service.exchange == self.exchange) & (Service.instrument == self.instrument))
+        return orderbook_details.address
+
+    def refresh_orderbook(self):
+        shm = self.get_orderbook_shm()
+        super().init_shm(shm)
