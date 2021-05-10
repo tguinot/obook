c20c4bb6a63054788e1ff7f2023fe39a363e2015
tguinot <thomas.guinot@gmail.com>
2021-05-10 14:43:20 +0000

    Now using db service for resilient orderbook reader

--- resilient_orderbook_reader.py
+++ resilient_orderbook_reader.py
@@ -1,5 +1,5 @@
 from liborderbook.orderbook_helper import RtOrderbookReader
-from models import OrderbookRecord, Currency, Exchange, Service
+from db_inquirer import DatabaseQuerier
 
 class ResilientOrderbookReader(RtOrderbookReader):
 
@@ -10,8 +10,11 @@ class ResilientOrderbookReader(RtOrderbookReader):
         super().__init__(shm)
 
     def get_orderbook_shm(self):
-        orderbook_details = Service.get((Service.name == 'OrderbookFeeder') & (Service.exchange == self.exchange) & (Service.instrument == self.instrument))
-        return orderbook_details.address
+        db_service_interface = DatabaseQuerier('127.0.0.1', 5678)
+        orderbook_details = db_service_interface.find_service('OrderbookFeeder', instrument=self.instrument, exchange=self.exchange) 
+        #orderbook_details = Service.get((Service.name == 'OrderbookFeeder') & (Service.exchange == self.exchange) & (Service.instrument == self.instrument))
+        db_service_interface.close()
+        return orderbook_details.get('address')
 
     def refresh_orderbook(self):
         shm = self.get_orderbook_shm()
